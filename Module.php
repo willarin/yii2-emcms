a4adc13f868b7cb19b3ae61f14e42b74c43ecd77
Dmitriy <dmitriy@almeydahost.com>
2015-02-23 13:17:46 +0400

    Standalone views final version

--- Module.php
+++ Module.php
@@ -8,20 +8,6 @@ namespace almeyda\emcms;
  */
 class Module extends \yii\base\Module
 {
-	/**
-     * @var mixed string the directory storing the theme files in the application. 
-     */
-    public $appThemePath = '';
-	
-	/**
-     * @var string a string to be prefixed to the user-specified view name to form a path to view folder.
-     */
-    public $viewsFolder = 'pages';
-    
-	/**
-     * @var string the relative path to layout
-     */
-    public $layoutPath = 'layouts/main.php';
 	
 	/**
      * @inheritdoc
@@ -29,6 +15,5 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
-		
-    }
+	}
 }
--- controllers/PageController.php
+++ controllers/PageController.php
@@ -4,52 +4,19 @@ namespace almeyda\emcms\controllers;
 
 use Yii;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\base\InvalidParamException;
 
 
 class PageController extends Controller
 {
-	public $defaultAction = 'page';
 	
 	public function actions()
     {
         return [
             'page' => [
                 'class' => 'yii\web\ViewAction',
-				'viewPrefix' => $this->module->appThemePath.$this->module->viewsFolder,
-				'layout' => $this->module->appThemePath.$this->module->layoutPath,
             ],
         ];
     }
-	
-	/**
-     * setup theme map used for the application
-     * @param Action $action the action to be executed.
-     * @return boolean whether the action should continue to be executed.
-     */
-    public function beforeAction($action)
-    {
-        /*
-		if ($this->module->appThemePath) {
-			$this->getView()->theme = Yii::createObject([
-				'class' => '\yii\base\Theme',
-				'pathMap' => ['@app/views' => $this->module->appThemePath],
-				'baseUrl' => $this->module->webThemePath,
-			]);
-		}
-		 * */
-		return true;
-    }
-	
-	public function actionIlndex($page = 'index')
-    {
-        try {
-		    return $this->render('//page/'.$page); 
-		} catch (InvalidParamException $e) {
-            throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'), $e->getCode(), $e);
-        }
-		
-    }
+
 	
 }
